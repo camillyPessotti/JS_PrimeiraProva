4d4fcb7a4ec3cd4f628e1e0db9bce1f9787512a3
Otavio <92130029+camillyPessotti@users.noreply.github.com>
2021-12-02 19:24:28 -0300

    commit

--- pokemonPage/pokemon.js
+++ pokemonPage/pokemon.js
@@ -1,6 +1,6 @@
-let pokemon = document.location.search;
+let id = document.location.search;
 
-pokemon = pokemon.substring(1);
+id = id.substring(1);
 
 let header = document.querySelector('header');
 
@@ -16,18 +16,77 @@ logo.appendChild(imagem);
 
 let main = document.querySelector('main');
 
-fetch(`https://prof-poke-api.herokuapp.com/api/pokemon/${id}/`)
+fetch(`https://prof-poke-api.herokuapp.com/api/pokemon/${id}`)
     .then(function(resultado) {
-        resultado.json().then(function(infos) {
-            infos.forEach(function(e) {
-                mostrarInfos(e.url_icon, e.url_icon_2, e.name.e.atk, e.def, e.atks, e.defs);
-            });
+        resultado.json().then(function(data) {
+            mostrarDados(data.url_icon, data.url_icon_2, data.name, data.atk, data.def, data.atks, data.defs);;
+
         });
     }).catch(function(error) {
         console.log("Error: ", error);
     });
 
+
+
 function mostrarDados(url_icon, url_icon_2, name, atk, def, atks, defs) {
+    let divNome = document.createElement('div');
+    divNome.className = 'divNome';
+    main.appendChild(divNome);
+
+
+    let nome = document.createElement('p');
+    nome.innerText = name;
+    nome.className = 'nome';
+    divNome.appendChild(nome);
+
+
+    let divIcon = document.createElement('div');
+    divIcon.className = 'divIcon';
+    main.appendChild(divIcon);
+
+
     let icon = document.createElement('img')
     icon.src = url_icon;
+    icon.style.width = '50%';
+
+    icon.onerror = function() {
+        icon.src = url_icon_2;
+    }
+
+    divIcon.appendChild(icon);
+
+    let divPrincipal = document.createElement('div');
+    divPrincipal.className = 'divPrincipal';
+    main.appendChild(divPrincipal);
+
+
+    let divNormal = document.createElement('div');
+    divNormal.className = 'divNormal';
+    divPrincipal.appendChild(divNormal);
+
+
+    let ataque = document.createElement('p');
+    ataque.innerText = 'Ataque: ' + atk;
+    divNormal.appendChild(ataque);
+
+
+    let defesa = document.createElement('p');
+    defesa.innerText = 'Defesa: ' + def;
+    divNormal.appendChild(defesa);
+
+
+
+    let divEspecial = document.createElement('div');
+    divEspecial.className = 'divEspecial';
+    divPrincipal.appendChild(divEspecial);
+
+
+    let ataqueEspecial = document.createElement('p');
+    ataqueEspecial.innerText = 'Ataque especial: ' + atks;
+    divEspecial.appendChild(ataqueEspecial);
+
+
+    let defesaEspecial = document.createElement('p');
+    defesaEspecial.innerText = 'Defesa especial: ' + defs;
+    divEspecial.appendChild(defesaEspecial);
 }
